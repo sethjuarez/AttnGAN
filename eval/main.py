949c90d7b8d60187956330fb51266b752bddec18
sethjuarez <me@sethjuarez.com>
2018-11-30 09:31:33 -0800

    added additional telemetry handling

--- eval/main.py
+++ eval/main.py
@@ -12,6 +12,8 @@ from miscc.config import cfg
 enable(os.environ["TELEMETRY"])
 app = Flask(__name__)
 app.wsgi_app = WSGIApplication(os.environ["TELEMETRY"], app.wsgi_app)
+tc = TelemetryClient(os.environ["TELEMETRY"])
+
 
 @app.route('/api/v1.0/bird', methods=['POST'])
 def create_bird():
@@ -20,20 +22,26 @@ def create_bird():
 
     caption = request.json['caption']
 
-    t0 = time.time()
-    urls = generate(caption, wordtoix, ixtoword, text_encoder, netG, blob_service)
-    t1 = time.time()
+    try:
+        t0 = time.time()
+        urls = generate(caption, wordtoix, ixtoword, text_encoder, netG, blob_service)
+        t1 = time.time()
 
-    response = {
-        'small': urls[0],
-        'medium': urls[1],
-        'large': urls[2],
-        'map1': urls[3],
-        'map2': urls[4],
-        'caption': caption,
-        'elapsed': t1 - t0
-    }
-    return jsonify({'bird': response}), 201
+        response = {
+            'small': urls[0],
+            'medium': urls[1],
+            'large': urls[2],
+            'map1': urls[3],
+            'map2': urls[4],
+            'caption': caption,
+            'elapsed': t1 - t0
+        }
+        return jsonify({'bird': response}), 201
+    except:
+        tc.track_exception(*sys.exc_info(), properties={ 'caption': caption })
+        tc.flush()
+        sys.exc_clear()
+        abort(500)
 
 @app.route('/api/v1.0/birds', methods=['POST'])
 def create_birds():
@@ -42,25 +50,31 @@ def create_birds():
 
     caption = request.json['caption']
 
-    t0 = time.time()
-    urls = generate(caption, wordtoix, ixtoword, text_encoder, netG, blob_service, copies=6)
-    t1 = time.time()
+    try:
+        t0 = time.time()
+        urls = generate(caption, wordtoix, ixtoword, text_encoder, netG, blob_service, copies=6)
+        t1 = time.time()
 
-    response = {
-        'bird1' : { 'small': urls[0], 'medium': urls[1], 'large': urls[2] },
-        'bird2' : { 'small': urls[3], 'medium': urls[4], 'large': urls[5] },
-        'bird3' : { 'small': urls[6], 'medium': urls[7], 'large': urls[8] },
-        'bird4' : { 'small': urls[9], 'medium': urls[10], 'large': urls[11] },
-        'bird5' : { 'small': urls[12], 'medium': urls[13], 'large': urls[14] },
-        'bird6' : { 'small': urls[15], 'medium': urls[16], 'large': urls[17] },
-        'caption': caption,
-        'elapsed': t1 - t0
-    }
-    return jsonify({'bird': response}), 201
+        response = {
+            'bird1' : { 'small': urls[0], 'medium': urls[1], 'large': urls[2] },
+            'bird2' : { 'small': urls[3], 'medium': urls[4], 'large': urls[5] },
+            'bird3' : { 'small': urls[6], 'medium': urls[7], 'large': urls[8] },
+            'bird4' : { 'small': urls[9], 'medium': urls[10], 'large': urls[11] },
+            'bird5' : { 'small': urls[12], 'medium': urls[13], 'large': urls[14] },
+            'bird6' : { 'small': urls[15], 'medium': urls[16], 'large': urls[17] },
+            'caption': caption,
+            'elapsed': t1 - t0
+        }
+        return jsonify({'bird': response}), 201
+    except:
+        tc.track_exception(*sys.exc_info(), properties={ 'caption': caption })
+        tc.flush()
+        sys.exc_clear()
+        abort(500)
 
 @app.route('/', methods=['GET'])
 def get_bird():
-    return 'Version 1'
+    return 'Version 2'
 
 if __name__ == '__main__':
     t0 = time.time()
